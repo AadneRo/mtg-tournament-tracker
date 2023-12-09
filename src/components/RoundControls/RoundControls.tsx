b2cc7df1957d2e9cbfbfe0d8082f9a07daa34d29
Ådne Rosenvinge <41082005+AadneRo@users.noreply.github.com>
2023-12-09 16:55:17 +0000

    feat: Add PlayerList and round start functionality
    Added PlayerList component and functionality to start the first round

--- src/components/RoundControls/RoundControls.tsx
+++ src/components/RoundControls/RoundControls.tsx
@@ -1,11 +1,25 @@
 import { useContext } from "react";
-import { Player } from "../../types";
 import { DataContext } from "../../services/Provider";
+import PlayerList from "../PlayerList";
+import { Button, P } from "@mui/material";
 
 function RoundControls() {
-  const { players } = useContext(DataContext);
+  const { players, rounds, nextRound } = useContext(DataContext);
 
-  return <div></div>;
+  return (
+    <div>
+      {rounds.length === 0 && (
+        <div>
+          <P>
+            No round has been started, do you want to start the first round with
+            the players bellow?
+          </P>
+          <Button onClick={nextRound}>Start Round</Button>
+        </div>
+      )}
+      <PlayerList players={players} canEdit={false} />
+    </div>
+  );
 }
 
 export default RoundControls;
